e00605fef077d472f23693ee1e390c9e3005bfd2
九门唐大佛爷 <1446046689@qq.com>
2020-07-31 14:32:32 +0800

    加减速和左右移动接口

--- Tetris/Tetris.js
+++ Tetris/Tetris.js
@@ -6,55 +6,55 @@ function Tetris(options) {
         blockNum: 20,  // 网格数量
         span: 2,       // 网格边框
         colories:      // 颜色列表
-        [
-            "#000000",
-            "#FF0000",
-            "#FFFF00",
-            "#FFFFFF",
-            "#0000FF",
-            "#00FFFF",
-            "#FF00FF"
-        ],
+            [
+                "#000000",
+                "#FF0000",
+                "#FFFF00",
+                "#FFFFFF",
+                "#0000FF",
+                "#00FFFF",
+                "#FF00FF"
+            ],
         boxShapes:  // 方块类型
-        [
-            // 土字型方块
             [
-                [0, 1, 0],
-                [1, 1, 1],
+                // 土字型方块
+                [
+                    [0, 1, 0],
+                    [1, 1, 1],
+                ],
+                // 条形方块
+                [[1, 1, 1, 1]],
+                // 左折型方块
+                [
+                    [1, 1, 0],
+                    [0, 1, 1],
+                ],
+                // 右折型方块
+                [
+                    [0, 1, 1],
+                    [1, 1, 0],
+                ],
+                // 倒左勾型方块
+                [
+                    [1, 0, 0],
+                    [1, 1, 1],
+                ],
+                // 正左勾型方块
+                [
+                    [1, 1, 1],
+                    [1, 0, 0],
+                ],
+                // 倒右勾型方块
+                [
+                    [0, 0, 1],
+                    [1, 1, 1],
+                ],
+                // 正右勾型方块
+                [
+                    [1, 1, 1],
+                    [0, 0, 1],
+                ],
             ],
-            // 条形方块
-            [[1, 1, 1, 1]],
-            // 左折型方块
-            [
-                [1, 1, 0],
-                [0, 1, 1],
-            ],
-            // 右折型方块
-            [
-                [0, 1, 1],
-                [1, 1, 0],
-            ],
-            // 倒左勾型方块
-            [
-                [1, 0, 0],
-                [1, 1, 1],
-            ],
-            // 正左勾型方块
-            [
-                [1, 1, 1],
-                [1, 0, 0],
-            ],
-            // 倒右勾型方块
-            [
-                [0, 0, 1],
-                [1, 1, 1],
-            ],
-            // 正右勾型方块
-            [
-                [1, 1, 1],
-                [0, 0, 1],
-            ],
-        ],
     };
     // 用户传入的参数覆盖默认参数
     for (let k in options) {
@@ -81,7 +81,7 @@ Tetris.prototype.init = function () {
         this.opt.span * (this.opt.blockNum + 1);
     document.body.appendChild(this.canves);
     this.ctx = this.canves.getContext("2d");         // 创建画笔
-    
+
     // 初始化网格
     for (let i = 0; i < this.opt.blockNum; i++) {
         this.gurid[i] = [];
@@ -99,11 +99,23 @@ Tetris.prototype.render = function () {
 
     // 记录当前时间
     let curTime = new Date().getTime();
-    
+
     // 通过判断两次动画的时间间隔来控制播放速度
     if (curTime - this.endTime > this.opt.speed * 1000) {
+        // 更新方块运动位置
         this.unpdate();
 
+        // 绘制表格
+        this.draw();
+
+        // 记录本次动画结束的时间
+        this.endTime = new Date().getTime();
+    }
+};
+
+// Tetris表格绘制函数
+Tetris.prototype.draw = function () {
+    if(this.gurid){
         for (let r = 0; r < this.gurid.length; r++) {
             for (let c = 0; c < this.gurid[r].length; c++) {
                 this.ctx.fillStyle = this.opt.colories[this.gurid[r][c]];
@@ -114,49 +126,44 @@ Tetris.prototype.render = function () {
                 this.ctx.fillRect(x, y, w, h);
             }
         }
-        // 判断是否碰到网格底部或者碰到已经停止运动的方块
-        if (this.judgeFinish()) {
-            //碰到底部，停止运动，并记录停止的方块
-            this.recordFinished();
-        }
-        // 记录本次动画结束的时间
-        this.endTime = new Date().getTime();
     }
-};
+    // 判断是否碰到网格底部或者碰到已经停止运动的方块
+    if (this.judgeFinish()) {
+        //碰到底部，停止运动，并记录停止的方块
+        this.recordFinished();
+    }
+}
 
 // Tetris状态更新函数（移动方块）
 Tetris.prototype.unpdate = function () {
     if (this.curBlock) {
+        this.clearBeforeBlock();
         // 存在下落的方块，就更新方块位置
         if (this.curBlock.data && this.curBlock.data.length > 0) {
             let arr = this.curBlock.data;
             for (let r = 0; r < arr.length; r++) {
                 if (arr[r] != null && arr[r].length) {
                     for (let c = 0; c < arr[r].length; c++) {
-                        // 清空上一行的颜色
-                        if (arr[r][c]&&r===0) {
-                            if (this.curBlock.y - 1 > -1) {
-                                this.gurid[this.curBlock.y - 1][this.curBlock.x + c] = 0;
-                            }
+                        if (arr[r][c]) {
+                            // 移动方块
+                            this.gurid[this.curBlock.y + r][
+                                this.curBlock.x + c
+                            ] = this.curBlock.colorIndex;
                         }
-                        // 移动方块
-                        this.gurid[this.curBlock.y + r][
-                            this.curBlock.x + c
-                        ] =(arr[r][c]===1?this.curBlock.colorIndex:0);
                     }
                 } else {
                     // 清空上一行的颜色
-                    if (arr[r]&&r===0) {
-                        if (this.curBlock.y  - 1 > -1) {
-                            this.gurid[this.curBlock.y - 1][this.curBlock.x + c] = 0;
-                        }
+                    if (arr[r]) {
+                        // 移动方块
+                        this.gurid[this.curBlock.y + r][
+                            this.curBlock.x + c
+                        ] = this.curBlock.colorIndex;
                     }
-                     // 移动方块
-                    this.gurid[this.curBlock.y + r][
-                        this.curBlock.x + c
-                    ] =(arr[r][c]===1?this.curBlock.colorIndex:0);
                 }
             }
+            // 下落前记录下落前的位置
+            this.curBlock.oldy = this.curBlock.y;
+            this.curBlock.oldx = this.curBlock.x;
             // 下落一行
             this.curBlock.y++;
         }
@@ -169,10 +176,12 @@ Tetris.prototype.unpdate = function () {
 // Tetris创建下落方块
 Tetris.prototype.createBlock = function () {
     this.curBlock = {
-        data: null, // 下落方块的形状
-        x: 0, // 下落方块的横向位置
-        y: 0, // 下落方块的纵向位置
-        colorIndex: 0, // 下落方块的颜色
+        data: null,    // 运动方块的形状
+        x: 0,          // 运动方块的当前位置的横向位置
+        y: 0,          // 运动方块的当前位置的纵向位置
+        oldx: 0,        // 运动方块的前一个位置的横向坐标
+        oldy: 0,        // 运动方块的前一个位置的纵向坐标
+        colorIndex: 0, // 运动方块的颜色
     };
 
     // 获取方块的随机颜色
@@ -195,10 +204,36 @@ Tetris.prototype.createBlock = function () {
     this.curBlock.x = Math.floor(
         (this.opt.blockNum - this.curBlock.data[0].length) / 2
     );
-    
+
     // 设置方块的左上角的纵坐标
     this.curBlock.y = 0;
 };
+
+// 清除移动之前位置的方块
+Tetris.prototype.clearBeforeBlock = function () {
+    if (this.curBlock) {
+        if (this.curBlock.data && this.curBlock.data.length > 0) {
+            let arr = this.curBlock.data;
+            for (let r = 0; r < arr.length; r++) {
+                if (arr[r] != null && arr[r].length) {
+                    for (let c = 0; c < arr[r].length; c++) {
+                        if (arr[r][c]) {
+                            this.gurid[this.curBlock.oldy + r][
+                                this.curBlock.oldx + c
+                            ] = 0;
+                        }
+                    }
+                } else {
+                    if (arr[r]) {
+                        this.gurid[this.curBlock.oldy + r][
+                            this.curBlock.oldx + c
+                        ] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
 
 // Tetris判断方块到底底部
 Tetris.prototype.judgeFinish = function () {
@@ -266,4 +301,58 @@ Tetris.prototype.recordFinished = function () {
         // 置空当前方块
         this.curBlock = null;
     }
+}
+
+// Tetris运动方块向左移动n格(默认移动一格)
+Tetris.prototype.moveLeft = function (n) {
+    let step = Number(n) || 1;
+    if (this.curBlock) {
+        if ((this.curBlock.x - step) < 0) {
+            console.log(`方块已经无法向左移动${step}格`);
+            return;
+        }
+        this.curBlock.oldx = this.curBlock.x;
+        this.curBlock.x = this.curBlock.x - step;
+        // 判断是否碰撞
+        if(this.judgeFinish()){
+            this.curBlock.x=this.curBlock.oldx;
+            return;
+        }
+        // 立刻更新网格
+        this.unpdate();
+        this.draw();
+    }
+}
+
+// Tetris运动方块向右移动n格(默认移动一格)
+Tetris.prototype.moveRight = function (n) {
+    let step = Number(n) || 1;
+    if (this.curBlock) {
+        if ((this.curBlock.x + (this.curBlock.data[0].length - 1) + step) > (this.opt.blockNum - 1)) {
+            console.log(`方块已经无法向右移动${step}格`);
+            return;
+        }
+        this.curBlock.oldx = this.curBlock.x;
+        this.curBlock.x = this.curBlock.x + step;
+
+        // 判断是否碰撞
+        if(this.judgeFinish()){
+            this.curBlock.x=this.curBlock.oldx;
+            return;
+        }
+        // 立刻更新网格
+        this.unpdate();
+        this.draw();
+    }
+}
+
+// Tetris运行方块减速
+Tetris.prototype.raiseUp = function (speed) {
+    this.opt.speed = this.opt.speed - (speed || 0.5);
+    this.opt.speed = (this.opt.speed < 0 ? 0 : this.opt.speed);
+}
+
+// Tetris运动方块加速
+Tetris.prototype.slowDown = function (speed) {
+    this.opt.speed = this.opt.speed + (speed || 0.5);
 }
--- Tetris/index.js
+++ Tetris/index.js
@@ -3,11 +3,33 @@ var tetris = null;
 window.addEventListener("load", function () {
   let maxHeight = document.body.clientHeight;
   let size = 15; // 方格大小
-  let span=1;    // 边框大小
-  let num = Math.floor((maxHeight-span) / (size+span));  // 计算方格数量
+  let span = 1;    // 边框大小
+  let num = Math.floor((maxHeight - span) / (size + span));  // 计算方格数量
   tetris = new Tetris({
     blockSize: size,
     blockNum: num,
     span: 1,
   });
 });
+
+window.addEventListener('keydown', function (evet) {
+  let keyCode = event.keyCode;
+  if (tetris) {
+    switch (keyCode) {
+      case 37:
+      case 65:  // 左
+        tetris.moveLeft();
+        break;
+      case 39:
+      case 68:  // 右
+        tetris.moveRight();
+        break;
+      case 187:  // 加速
+        tetris.raiseUp();
+        break;
+      case 189:  // 减速
+        tetris.slowDown();
+        break;
+    }
+  }
+})
